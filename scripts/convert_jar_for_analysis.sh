b0da21c16d7e6f8b5f268b0e94fd02b0b86eb165
Dominik Gabi <dominik@fb.com>
2020-12-04 16:36:35 -0800

    setup and apply license linter
    Reviewed By: zertosh
    
    Differential Revision: D25336789
    
    fbshipit-source-id: b8bd6f8e6a0b3d0768329213fcd1249f26b92d12

--- scripts/convert_jar_for_analysis.sh
+++ scripts/convert_jar_for_analysis.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 
 D8=/opt/android/sdk_D23134735/build-tools/29.0.2/d8
 $D8 --min-api 24 --output classes_for_analysis.zip "$@"
