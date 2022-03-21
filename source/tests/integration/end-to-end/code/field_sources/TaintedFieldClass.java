4573b7dbbf46430732696f0adf01b36fd6c735c3
Simran Virk <skvirk@fb.com>
2022-03-21 09:38:08 -0700

    Add test case and wiki note to explain limitation of field sources
    Summary:
    As explained in the test case, final primitive typed fields are optimized to a const access rather than a field access by the java compiler.
    
    The workaround for this would be to change the analyzed source code to use a getter
    
    Reviewed By: arthaud
    
    Differential Revision: D34953088
    
    fbshipit-source-id: a62b10f91237af3b255ca2fe1d7a0c7403d19b4e

--- source/tests/integration/end-to-end/code/field_sources/TaintedFieldClass.java
+++ source/tests/integration/end-to-end/code/field_sources/TaintedFieldClass.java
@@ -11,9 +11,29 @@ public class TaintedFieldClass {
   public Object field;
   public Object shadowedField;
   public static Object staticField;
+  public static final Object staticFinalField = new Object();
+
+  public static String staticStringField;
+  public final String finalStringField = "final";
+  public static final String staticFinalStringField = "Source";
 
   public void flow() {
     Origin.sink(field);
     Origin.sink(staticField);
+    Origin.sink(staticFinalField);
+
+    stringSink(staticStringField);
+    // If a field is final and primitive, then we don't see issues from it
+    // since the sget (field access) opcode is optimized by the java compiler
+    // into const/const_string opcodes loading the value of the field.
+    //
+    // Note that a final field that is not primitive is not optimized in this way
+    // so we still see an issue from staticFinalField above.
+    stringSink(finalStringField);
+    stringSink(staticFinalStringField);
+  }
+
+  public void stringSink(String argument) {
+    return;
   }
 }
