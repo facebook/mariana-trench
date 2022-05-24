3973f047446182977809ea4ae2b76153a35943f8
Anwesh Tuladhar <atuladhar@fb.com>
2022-05-24 16:11:17 -0700

    Fix opensource
    Summary:
    Fix build error: https://github.com/facebook/mariana-trench/runs/6562489546?check_suite_focus=true
    ```
    /Users/runner/work/mariana-trench/mariana-trench/source/tests/integration/json-model-generator/IntegrationTest.cpp:126:1: error: no viable conversion from returned value of type 'boost::filesystem::path' to function return type '::std::string' (aka 'basic_string<char>')
    MT_INSTANTIATE_TEST_SUITE_P( ...
    ```
    
    Reviewed By: yuhshin-oss
    
    Differential Revision: D36639695
    
    fbshipit-source-id: 8ad7beea166cce70698e4c1c79f8ba2b0b2b8e1f

--- source/tests/Test.h
+++ source/tests/Test.h
@@ -87,15 +87,17 @@ std::vector<std::string> sub_directories(
  */
 std::string normalize_json_lines(const std::string& input);
 
-#define MT_INSTANTIATE_TEST_SUITE_P(                                          \
-    INSTANTIATION_NAME, TEST_SUITE_NAME, PARAM_GENERATOR)                     \
-  INSTANTIATE_TEST_SUITE_P(                                                   \
-      INSTANTIATION_NAME,                                                     \
-      TEST_SUITE_NAME,                                                        \
-      PARAM_GENERATOR,                                                        \
-      ([](const ::testing::TestParamInfo<TEST_SUITE_NAME::ParamType>& info) { \
-        boost::filesystem::path name = info.param;                            \
-        return name.stem();                                                   \
+#define MT_INSTANTIATE_TEST_SUITE_P(                                        \
+    INSTANTIATION_NAME, TEST_SUITE_NAME, PARAM_GENERATOR)                   \
+  INSTANTIATE_TEST_SUITE_P(                                                 \
+      INSTANTIATION_NAME,                                                   \
+      TEST_SUITE_NAME,                                                      \
+      PARAM_GENERATOR,                                                      \
+      ([](const ::testing::TestParamInfo<TEST_SUITE_NAME::ParamType>& info) \
+           -> std::string {                                                 \
+        boost::filesystem::path name = info.param;                          \
+        std::string test_name = name.stem().string();                       \
+        return test_name;                                                   \
       }));
 
 } // namespace test
