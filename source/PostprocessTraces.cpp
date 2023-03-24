47d772d5fdaedad67f2477fdb90f8b1caed6f394
Anwesh Tuladhar <atuladhar@meta.com>
2023-03-24 16:08:40 -0700

    Allow transforms in final models
    Summary: `PostprocessTraces` checks for the kinds to match from caller to callee. With transformed kinds, these will defer. This diff adds logic to compute the correct kinds to look for in the callee so that the inferred models are not dropped.
    
    Reviewed By: arthaud
    
    Differential Revision: D44121968
    
    fbshipit-source-id: 35db78da84b9f46d754fb661f8db2deadc223d51

--- source/PostprocessTraces.cpp
+++ source/PostprocessTraces.cpp
@@ -16,7 +16,49 @@ namespace marianatrench {
 
 namespace {
 
+bool check_callee_kinds(
+    const Context& context,
+    const Kind* kind,
+    const Taint& callee_taint) {
+  const auto* transform_kind = kind->as<TransformKind>();
+  if (transform_kind == nullptr) {
+    return callee_taint.contains_kind(kind);
+  }
+
+  const auto* base_kind = transform_kind->base_kind();
+  const auto* global_transforms = transform_kind->global_transforms();
+  if (global_transforms == nullptr) {
+    // transform_kind only has local transforms, in which case we can just check
+    // for the base_kind in the callee.
+    return callee_taint.contains_kind(base_kind);
+  }
+
+  for (const auto& frame : callee_taint.frames_iterator()) {
+    const auto* frame_kind = frame.kind();
+    if (frame_kind == nullptr) {
+      continue;
+    }
+
+    const auto* frame_transform_kind = frame_kind->as<TransformKind>();
+    if (frame_transform_kind == nullptr ||
+        frame_transform_kind->base_kind() != base_kind) {
+      // No match
+      continue;
+    }
+
+    if (global_transforms ==
+        context.transforms->concat(
+            frame_transform_kind->local_transforms(),
+            frame_transform_kind->global_transforms())) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 bool is_valid_generation(
+    const Context& context,
     const Method* MT_NULLABLE callee,
     const AccessPath& callee_port,
     const Kind* kind,
@@ -32,11 +74,13 @@ bool is_valid_generation(
     return true;
   }
   auto model = registry.get(callee);
-  auto sources = model.generations().raw_read(callee_port).root();
-  return sources.contains_kind(kind);
+
+  return check_callee_kinds(
+      context, kind, model.generations().raw_read(callee_port).root());
 }
 
 bool is_valid_sink(
+    const Context& context,
     const Method* MT_NULLABLE callee,
     const AccessPath& callee_port,
     const Kind* kind,
@@ -57,7 +101,7 @@ bool is_valid_sink(
   auto model = registry.get(callee);
   auto sinks = model.sinks().raw_read(callee_port).root();
 
-  if (sinks.contains_kind(kind)) {
+  if (check_callee_kinds(context, kind, sinks)) {
     return true;
   }
 
@@ -65,7 +109,9 @@ bool is_valid_sink(
   // could be a PartialKind that turned into a TriggeredPartialKind in the
   // caller (sink_kind). The sink is valid as long as its underlying partial
   // kind matches that of its callee's.
-  const auto* sink_triggered_kind = kind->as<TriggeredPartialKind>();
+  // Transforms are not supported for PartialKinds
+  const auto* sink_triggered_kind =
+      kind->discard_transforms()->as<TriggeredPartialKind>();
   if (!sink_triggered_kind) {
     return false;
   }
@@ -74,42 +120,47 @@ bool is_valid_sink(
 }
 
 TaintAccessPathTree cull_collapsed_generations(
+    const Context& context,
     TaintAccessPathTree generation_tree,
     const Registry& registry) {
   generation_tree.map([&](Taint& generation_taint) {
     generation_taint.filter_invalid_frames([&](const Method* MT_NULLABLE callee,
                                                const AccessPath& callee_port,
                                                const Kind* kind) {
-      return is_valid_generation(callee, callee_port, kind, registry);
+      return is_valid_generation(context, callee, callee_port, kind, registry);
     });
   });
   return generation_tree;
 }
 
 TaintAccessPathTree cull_collapsed_sinks(
+    const Context& context,
     TaintAccessPathTree sink_tree,
     const Registry& registry) {
   sink_tree.map([&](Taint& sink_taint) {
     sink_taint.filter_invalid_frames([&](const Method* MT_NULLABLE callee,
                                          const AccessPath& callee_port,
                                          const Kind* kind) {
-      return is_valid_sink(callee, callee_port, kind, registry);
+      return is_valid_sink(context, callee, callee_port, kind, registry);
     });
   });
   return sink_tree;
 }
 
-IssueSet cull_collapsed_issues(IssueSet issues, const Registry& registry) {
+IssueSet cull_collapsed_issues(
+    const Context& context,
+    IssueSet issues,
+    const Registry& registry) {
   issues.map([&](Issue& issue) {
     issue.filter_sources([&](const Method* MT_NULLABLE callee,
                              const AccessPath& callee_port,
                              const Kind* kind) {
-      return is_valid_generation(callee, callee_port, kind, registry);
+      return is_valid_generation(context, callee, callee_port, kind, registry);
     });
     issue.filter_sinks([&](const Method* MT_NULLABLE callee,
                            const AccessPath& callee_port,
                            const Kind* kind) {
-      return is_valid_sink(callee, callee_port, kind, registry);
+      return is_valid_sink(context, callee, callee_port, kind, registry);
     });
   });
   return issues;
@@ -136,10 +187,12 @@ void PostprocessTraces::remove_collapsed_traces(
           const auto old_model = registry.get(method);
 
           auto model = old_model;
-          model.set_generations(
-              cull_collapsed_generations(model.generations(), registry));
-          model.set_sinks(cull_collapsed_sinks(model.sinks(), registry));
-          model.set_issues(cull_collapsed_issues(model.issues(), registry));
+          model.set_generations(cull_collapsed_generations(
+              context, model.generations(), registry));
+          model.set_sinks(
+              cull_collapsed_sinks(context, model.sinks(), registry));
+          model.set_issues(
+              cull_collapsed_issues(context, model.issues(), registry));
 
           if (!old_model.leq(model)) {
             for (const auto* dependency :
