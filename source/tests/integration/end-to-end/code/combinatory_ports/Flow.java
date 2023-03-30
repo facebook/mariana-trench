3f7dff033aae55313a3057892a18800cfa82cd2e
Maxime Arthaud <arthaud@meta.com>
2023-03-30 07:49:59 -0700

    Add integration tests with recursive sinks
    Summary:
    This test shows what happens with recursive methods with sinks.
    On `recursive_parent` we infer sinks on `this`, `parent`, `parent.parent`, etc.
    On `recursive_praent_x` we infer sinks on `x`, `parent.x`, `parent.parent.x`, etc.
    This is unoptimal since those sinks are then propagated. This is detrimental to performance.
    
    In a follow-up, we will implement abstract tree shaping and molding to get rid of these duplicate sinks.
    
    Reviewed By: anwesht
    
    Differential Revision: D44496749
    
    fbshipit-source-id: 7246c3f4fc715c7a867ea6def08d3b526c4b2fe2

--- source/tests/integration/end-to-end/code/combinatory_ports/Flow.java
+++ source/tests/integration/end-to-end/code/combinatory_ports/Flow.java
@@ -8,6 +8,8 @@
 package com.facebook.marianatrench.integrationtests;
 
 public class Flow {
+  /* Combinatory explosion of ports using overrides. */
+
   private interface Base {
     void method();
   }
@@ -35,6 +37,29 @@ public class Flow {
     public void method() {
       Origin.sink(this.x);
       this.c.method();
+    }
+  }
+
+  /* Combinatory explosion using recursion. */
+
+  private class Node {
+    private Node parent;
+    private Object x;
+
+    public void recursive_parent() {
+      if (parent != null) {
+        this.parent.recursive_parent();
+      } else {
+        Origin.sink(this);
+      }
+    }
+
+    public void recursive_parent_x() {
+      if (parent != null) {
+        this.parent.recursive_parent_x();
+      } else {
+        Origin.sink(this.x);
+      }
     }
   }
 }
