a7d262d69b01a2676b0dba536ada3b815a091089
Yuh Shin Ong <yuhshin@meta.com>
2023-08-01 15:40:15 -0700

    Life-cycle test: Add clarifying comments
    Summary:
    Just documenting a false negative pattern in our life-cycle due to a limitation of which methods the life-cycle wrapper can call to.
    
    The hope is that with class intervals, life-cycle wrappers can call these methods without worrying about false positives due to flows from unrelated classes.
    
    Reviewed By: anwesht
    
    Differential Revision: D47726809
    
    fbshipit-source-id: 8cf5ca84840743db04e14489e0415743fc8869bd

--- source/tests/integration/end-to-end/code/fragment_lifecycle/ChildFragment.java
+++ source/tests/integration/end-to-end/code/fragment_lifecycle/ChildFragment.java
@@ -24,6 +24,15 @@ public class ChildFragment extends BaseFragment {
 
   @Override
   protected void onStart() {
+    // False negative:
+    // mTainted2 is tainted in onAfterActivityCreate() called from onCreate()
+    // but because this class does not override onCreate(), the call to
+    // onCreate() does not make it into the life-cycle wrapper and the analysis
+    // misses the flow.
+    // Suggested fix: Call base class methods when override does not exist.
+    // Needs class intervals to avoid false positives because the base class
+    // could have tainted fields from other derived classes unrelated to this
+    // one.
     super.onStart();
     Origin.sink(mTainted2);
   }
