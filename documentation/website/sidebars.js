fa844f90468f33f5d568f49c276044a16bfdaa04
Max Carmichael <mcarmichael@meta.com>
2023-10-06 20:13:30 -0700

    Edit order of sidebar
    Summary:
    Title.
    
    The three Running on Apps should be grouped together
    
    Configuration seems related to Running on Apps, so I moved it next to it
    
    Feature descriptions I initially thought was going to explain to me what features are; I think calling this a Glossary makes more sense
    
    XFN coming before Using Mariana Trench 101 makes more sense to me, but not a big deal
    
    Reviewed By: anwesht
    
    Differential Revision: D50016691
    
    fbshipit-source-id: 8fd8ffae9b544c20b1ea9e2471091d271b9f4e58

--- documentation/website/sidebars.js
+++ documentation/website/sidebars.js
@@ -7,36 +7,39 @@
  * @format
  */
 
-const {fbContent, fbInternalOnly} = require('docusaurus-plugin-internaldocs-fb/internal');
+const {
+  fbContent,
+  fbInternalOnly,
+} = require('docusaurus-plugin-internaldocs-fb/internal');
 
 module.exports = {
   docs: {
-  "User Guide": [
-    'overview',
-    fbContent({external: 'getting-started'}),
-    fbInternalOnly("fb/using-mt-101"),
-    fbInternalOnly("fb/xfn-onboarding"),
-    fbInternalOnly('fb/running-on-apps'),
-    fbInternalOnly('fb/running-on-3rd-party-apps'),
-    'customize-sources-and-sinks',
-    'models',
-    'shims',
-    'configuration',
-    'feature-descriptions',
-    'known-false-negatives',
-    fbInternalOnly("fb/testing-diffs"),
-    fbInternalOnly("fb/running-on-new-apps"),
-],
-  "Developer Guide": [
-    fbContent({external: 'contribution'}),
-    fbInternalOnly('fb/developer-getting-started'),
-    fbInternalOnly('fb/running-tests'),
-    fbInternalOnly('fb/inspecting-an-apk'),
-    fbInternalOnly('fb/android-lifecycles'),
-    'debugging-fp-fns',
-    fbInternalOnly('fb/deployment'),
-    fbInternalOnly('fb/continuous-and-diff-runs'),
-    fbInternalOnly("fb/crtex"),
-    ]
+    'User Guide': [
+      'overview',
+      fbContent({external: 'getting-started'}),
+      fbInternalOnly('fb/xfn-onboarding'),
+      fbInternalOnly('fb/using-mt-101'),
+      fbInternalOnly('fb/running-on-apps'),
+      fbInternalOnly('fb/running-on-new-apps'),
+      fbInternalOnly('fb/running-on-3rd-party-apps'),
+      'configuration',
+      'customize-sources-and-sinks',
+      'models',
+      'shims',
+      'feature-descriptions',
+      'known-false-negatives',
+      fbInternalOnly('fb/testing-diffs'),
+    ],
+    'Developer Guide': [
+      fbContent({external: 'contribution'}),
+      fbInternalOnly('fb/developer-getting-started'),
+      fbInternalOnly('fb/running-tests'),
+      fbInternalOnly('fb/inspecting-an-apk'),
+      fbInternalOnly('fb/android-lifecycles'),
+      'debugging-fp-fns',
+      fbInternalOnly('fb/deployment'),
+      fbInternalOnly('fb/continuous-and-diff-runs'),
+      fbInternalOnly('fb/crtex'),
+    ],
   },
 };
