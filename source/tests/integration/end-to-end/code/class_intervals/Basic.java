c909f7c3a1cc6097eca3d6def7ba2f10c7029cdd
Yuh Shin Ong <yuhshin@meta.com>
2023-12-18 22:50:09 -0800

    ClassIntervals: Add more tests for narrowing of intervals at this.* calls
    Summary: See title.
    
    Reviewed By: anwesht
    
    Differential Revision: D52142146
    
    fbshipit-source-id: c4bab40d7fb99c68e6c96bb4b7893e4b86cb23ac

--- source/tests/integration/end-to-end/code/class_intervals/Basic.java
+++ source/tests/integration/end-to-end/code/class_intervals/Basic.java
@@ -8,7 +8,16 @@
 package com.facebook.marianatrench.integrationtests;
 
 public class Basic {
+  static Object staticSource() {
+    return Origin.source();
+  }
+
+  static void staticSink(Object argument) {
+    Origin.sink(argument);
+  }
+
   abstract class Base {
+
     abstract Object potentialSource();
 
     abstract void potentialSink(Object argument);
@@ -16,6 +25,26 @@ public class Basic {
     abstract Object derived1Source();
 
     abstract void derived1Sink(Object argument);
+
+    abstract Object derived2SourceNotViaThis();
+
+    abstract void derived2SinkNotViaThis(Object argument);
+
+    Object baseSource() {
+      return Origin.source();
+    }
+
+    void baseSink(Object argument) {
+      Origin.sink(argument);
+    }
+
+    Object baseSourceNotViaThis() {
+      return Basic.staticSource();
+    }
+
+    void baseSinkNotViaThis(Object argument) {
+      Basic.staticSink(argument);
+    }
 
     void noIssue() {
       // Derived1's source cannot flow into Derived2's sink since they are
@@ -28,6 +57,23 @@ public class Basic {
     void truePositive() {
       Object o = this.derived1Source();
       this.derived1Sink(o);
+    }
+
+    void truePositive2() {
+      // The leaf source/sink are not class-specific. However, the flow is and
+      // class intervals are expected to reflect that of Derived2's.
+      Object o = this.derived2SourceNotViaThis();
+      this.derived2SinkNotViaThis(o);
+    }
+
+    void truePositive3() {
+      Object o = this.baseSource();
+      this.baseSink(o);
+    }
+
+    void truePositive4() {
+      Object o = this.baseSourceNotViaThis();
+      this.baseSinkNotViaThis(o);
     }
   }
 
@@ -42,12 +88,36 @@ public class Basic {
 
     @Override
     public Object derived1Source() {
+      // Specifically, we want preserves_type_context=True.
+      // Making this an origin frame achieves that.
       return Origin.source();
     }
 
     @Override
     public void derived1Sink(Object argument) {
       Origin.sink(argument);
+    }
+
+    @Override
+    public Object derived2SourceNotViaThis() {
+      return new Object();
+    }
+
+    @Override
+    public void derived2SinkNotViaThis(Object argument) {}
+
+    void truePositive() {
+      // Flows via base. However, as this is called from Derived1, intervals
+      // should be that of Derived1's.
+      Object o = this.baseSource();
+      this.baseSink(o);
+    }
+
+    void truePositive2() {
+      // Flows via base. However, as this is called from Derived1, intervals
+      // should be that of Derived1's.
+      Object o = this.baseSourceNotViaThis();
+      this.baseSinkNotViaThis(o);
     }
   }
 
@@ -69,5 +139,17 @@ public class Basic {
 
     @Override
     public void derived1Sink(Object argument) {}
+
+    @Override
+    public Object derived2SourceNotViaThis() {
+      // Specifically, we want preserves_type_context=False.
+      // Calling into a non-this.* method achieves that.
+      return Basic.staticSource();
+    }
+
+    @Override
+    public void derived2SinkNotViaThis(Object argument) {
+      Basic.staticSink(argument);
+    }
   }
 }
