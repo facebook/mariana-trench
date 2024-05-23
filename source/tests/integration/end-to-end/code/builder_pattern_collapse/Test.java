0f2dd11796a443eec248418ce25d0cf9af43ec43
Jinghao Jia <jinghao7@meta.com>
2024-05-23 12:10:25 -0700

    Config heuristics for builder pattern collapsing test
    Summary:
    As the title goes, this allows us to remove the excessive amount of fields we
    need to taint for the default `generation_max_output_path_leaves` value
    
    Reviewed By: anwesht
    
    Differential Revision: D57731291
    
    fbshipit-source-id: 87a30886035336e7f490acfdaebda630203e9d67

--- source/tests/integration/end-to-end/code/builder_pattern_collapse/Test.java
+++ source/tests/integration/end-to-end/code/builder_pattern_collapse/Test.java
@@ -8,77 +8,19 @@
 package com.facebook.marianatrench.integrationtests;
 
 public class Test {
-  // 22 fields, Heuristics::kGenerationMaxOutputPathLeaves = 20
+  // 4 fields, configure generation_max_output_path_leaves = 2
   public Test a;
   public Test b;
   public Test c;
   public Test d;
-  public Test e;
-  public Test f;
-  public Test g;
-  public Test h;
-  public Test i;
-  public Test j;
-  public Test k;
-  public Test l;
-  public Test m;
-  public Test n;
-  public Test o;
-  public Test p;
-  public Test q;
-  public Test r;
-  public Test s;
-  public Test t;
-  public Test u;
-  public Test v;
 
   public Test() {}
 
-  public Test(
-      Test a,
-      Test b,
-      Test c,
-      Test d,
-      Test e,
-      Test f,
-      Test g,
-      Test h,
-      Test i,
-      Test j,
-      Test k,
-      Test l,
-      Test m,
-      Test n,
-      Test o,
-      Test p,
-      Test q,
-      Test r,
-      Test s,
-      Test t,
-      Test u,
-      Test v) {
+  public Test(Test a, Test b, Test c, Test d) {
     this.a = a;
     this.b = b;
     this.c = c;
     this.d = d;
-    this.e = e;
-    this.f = f;
-    this.g = g;
-    this.h = h;
-    this.i = i;
-    this.j = j;
-    this.k = k;
-    this.l = l;
-    this.m = m;
-    this.n = n;
-    this.o = o;
-    this.p = p;
-    this.q = q;
-    this.r = r;
-    this.s = s;
-    this.t = t;
-    this.u = u;
-    this.v = v;
   }
 
   private static Object differentSource() {
@@ -92,24 +34,6 @@ public class Test {
     public Test b;
     public Test c;
     public Test d;
-    public Test e;
-    public Test f;
-    public Test g;
-    public Test h;
-    public Test i;
-    public Test j;
-    public Test k;
-    public Test l;
-    public Test m;
-    public Test n;
-    public Test o;
-    public Test p;
-    public Test q;
-    public Test r;
-    public Test s;
-    public Test t;
-    public Test u;
-    public Test v;
 
     public Builder setA(Test a) {
       this.a = a;
@@ -131,128 +55,20 @@ public class Test {
       return this;
     }
 
-    public Builder setE(Test e) {
-      this.e = e;
-      return this;
-    }
-
-    public Builder setF(Test f) {
-      this.f = f;
-      return this;
-    }
-
-    public Builder setG(Test g) {
-      this.g = g;
-      return this;
-    }
-
-    public Builder setH(Test h) {
-      this.h = h;
-      return this;
-    }
-
-    public Builder setI(Test i) {
-      this.i = i;
-      return this;
-    }
-
-    public Builder setJ(Test j) {
-      this.j = j;
-      return this;
-    }
-
-    public Builder setK(Test k) {
-      this.k = k;
-      return this;
-    }
-
-    public Builder setL(Test l) {
-      this.l = l;
-      return this;
-    }
-
-    public Builder setM(Test m) {
-      this.m = m;
-      return this;
-    }
-
-    public Builder setN(Test n) {
-      this.n = n;
-      return this;
-    }
-
-    public Builder setO(Test o) {
-      this.o = o;
-      return this;
-    }
-
-    public Builder setP(Test p) {
-      this.p = p;
-      return this;
-    }
-
-    public Builder setQ(Test q) {
-      this.q = q;
-      return this;
-    }
-
-    public Builder setR(Test r) {
-      this.r = r;
-      return this;
-    }
-
-    public Builder setS(Test s) {
-      this.s = s;
-      return this;
-    }
-
-    public Builder setT(Test t) {
-      this.t = t;
-      return this;
-    }
-
-    public Builder setU(Test u) {
-      this.u = u;
-      return this;
-    }
-
-    public Builder setV(Test v) {
-      this.v = v;
-      return this;
-    }
-
     public Test build() {
-      return new Test(
-          this.a, this.b, this.c, this.d, this.e, this.f, this.g, this.h, this.i, this.j, this.k,
-          this.l, this.m, this.n, this.o, this.p, this.q, this.r, this.s, this.t, this.u, this.v);
+      return new Test(this.a, this.b, this.c, this.d);
     }
   }
 
   public static Test collapseOnBuilder() {
     // Resulting taint tree will have caller port Return
+    // Taint 3 fields to trigger collapsing behavior,
+    // configure generation_max_output_path_leaves = 2
     return (new Builder())
         .setA((Test) Origin.source())
         .setB((Test) Test.differentSource())
         .setC((Test) Origin.source())
-        .setD((Test) Origin.source())
-        .setE((Test) Origin.source())
-        .setF((Test) Origin.source())
-        .setG((Test) Origin.source())
-        .setH((Test) Origin.source())
-        .setI((Test) Origin.source())
-        .setJ((Test) Origin.source())
-        .setK((Test) Origin.source())
-        .setL((Test) Origin.source())
-        .setM((Test) Origin.source())
-        .setN((Test) Origin.source())
-        .setO((Test) Origin.source())
-        .setP((Test) Origin.source())
-        .setQ((Test) Origin.source())
-        .setR((Test) Origin.source())
-        .setS((Test) Origin.source())
-        .setT((Test) Origin.source())
-        .setU((Test) Origin.source())
-        .build(); // 21 fields Heuristics::kGenerationMaxOutputPathLeaves = 20
+        .build();
   }
 
   public static Test noCollapseOnBuilder() {
@@ -270,8 +86,8 @@ public class Test {
     // Also issue: false positive due to collapsing
     Test.differentSink(output.a);
     Origin.sink(output.b);
-    Origin.sink(output.v);
-    Test.differentSink(output.v);
+    Origin.sink(output.d);
+    Test.differentSink(output.d);
   }
 
   public static void testNoCollapseOnBuilder() {
@@ -284,7 +100,7 @@ public class Test {
     // no issue, since no collapsing occurs
     Test.differentSink(output.a);
     Origin.sink(output.b);
-    Origin.sink(output.v);
-    Test.differentSink(output.v);
+    Origin.sink(output.d);
+    Test.differentSink(output.d);
   }
 }
