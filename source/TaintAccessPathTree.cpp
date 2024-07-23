13d4b48e265461b512224537cce44b05d483ac4b
Anwesh Tuladhar <atuladhar@meta.com>
2024-07-23 14:32:18 -0700

    Refactor TaintAccessPathTree to remove intermediate abstraction of AccessPathTaintTree
    Summary: Currently we have a generic abstraction of `AccessPathTreeDomain`. This is however only used for `TaintAccessPathTree` and is not necessary. It also complicates the implementation of overriding heuristics per Root. This diff flattens `TaintAccessPathTree` to directly wrap `RootPatriciaTreeAbstractPartition<TaintTree>` instead of the `AccessPathTreeDomain<Taint, TaintTreeConfiguration>`. There should be no changes in results.
    
    Reviewed By: arthaud
    
    Differential Revision: D60084922
    
    fbshipit-source-id: 02af1a0035c34a49aa94e1ac61c6b41603e0b306

--- source/TaintAccessPathTree.cpp
+++ source/TaintAccessPathTree.cpp
@@ -7,46 +7,69 @@
 
 #include <mariana-trench/FeatureFactory.h>
 #include <mariana-trench/TaintAccessPathTree.h>
+#include <mariana-trench/TaintTree.h>
 
 namespace marianatrench {
 
+TaintAccessPathTree::TaintAccessPathTree(
+    std::initializer_list<std::pair<AccessPath, Taint>> edges) {
+  for (const auto& [access_path, elements] : edges) {
+    write(access_path, elements, UpdateKind::Weak);
+  }
+}
+
 TaintTree TaintAccessPathTree::read(Root root) const {
-  return TaintTree(tree_.read(root));
+  return map_.get(root);
 }
 
 TaintTree TaintAccessPathTree::read(const AccessPath& access_path) const {
-  return TaintTree(tree_.read(access_path));
+  return map_.get(access_path.root()).read(access_path.path());
 }
 
 TaintTree TaintAccessPathTree::raw_read(const AccessPath& access_path) const {
-  return TaintTree(tree_.raw_read(access_path));
+  return map_.get(access_path.root()).raw_read(access_path.path());
 }
 
 void TaintAccessPathTree::write(
     const AccessPath& access_path,
     Taint taint,
     UpdateKind kind) {
-  tree_.write(access_path, std::move(taint), kind);
+  map_.update(
+      access_path.root(),
+      [&access_path, &taint, kind](const TaintTree& taint_tree) {
+        auto copy = taint_tree;
+        copy.write(access_path.path(), std::move(taint), kind);
+        return copy;
+      });
 }
 
 void TaintAccessPathTree::write(
     const AccessPath& access_path,
-    TaintTree tree,
+    TaintTree taint_tree,
     UpdateKind kind) {
-  tree_.write(access_path, std::move(tree.tree_), kind);
+  map_.update(
+      access_path.root(),
+      [&access_path, &taint_tree, kind](const TaintTree& subtree) {
+        auto copy = subtree;
+        copy.write(access_path.path(), std::move(taint_tree), kind);
+        return copy;
+      });
 }
 
 std::vector<std::pair<AccessPath, const Taint&>> TaintAccessPathTree::elements()
     const {
-  return tree_.elements();
+  std::vector<std::pair<AccessPath, const Taint&>> results;
+  visit([&results](const AccessPath& access_path, const Taint& element) {
+    results.push_back({access_path, element});
+  });
+  return results;
 }
 
-std::vector<std::pair<Root, TaintTree>> TaintAccessPathTree::roots() const {
-  // Since sizeof(TaintTree) != sizeof(AbstractTreeDomain<Taint>), we cannot
-  // return references to `TaintTree`s.
-  std::vector<std::pair<Root, TaintTree>> results;
-  for (const auto& [root, tree] : tree_) {
-    results.emplace_back(root, TaintTree(tree));
+std::vector<std::pair<Root, const TaintTree&>> TaintAccessPathTree::roots()
+    const {
+  std::vector<std::pair<Root, const TaintTree&>> results;
+  for (const auto& [root, taint_tree] : map_) {
+    results.emplace_back(root, taint_tree);
   }
   return results;
 }
@@ -54,11 +77,23 @@ std::vector<std::pair<Root, TaintTree>> TaintAccessPathTree::roots() const {
 void TaintAccessPathTree::limit_leaves(
     std::size_t max_leaves,
     const FeatureMayAlwaysSet& broadening_features) {
-  tree_.limit_leaves(max_leaves, [&broadening_features](Taint taint) {
-    taint.add_locally_inferred_features(broadening_features);
-    taint.update_maximum_collapse_depth(CollapseDepth::zero());
-    return taint;
+  map_.transform([max_leaves, &broadening_features](TaintTree taint_tree) {
+    taint_tree.limit_leaves(max_leaves, broadening_features);
+    return taint_tree;
   });
+}
+
+std::ostream& operator<<(std::ostream& out, const TaintAccessPathTree& tree) {
+  out << "TaintAccessPathTree {";
+  for (auto iterator = tree.map_.begin(), end = tree.map_.end();
+       iterator != end;) {
+    out << iterator->first << " -> " << iterator->second;
+    ++iterator;
+    if (iterator != end) {
+      out << ", ";
+    }
+  }
+  return out << "}";
 }
 
 } // namespace marianatrench
--- source/TaintAccessPathTree.h
+++ source/TaintAccessPathTree.h
@@ -18,35 +18,37 @@ namespace marianatrench {
 class TaintAccessPathTree final
     : public sparta::AbstractDomain<TaintAccessPathTree> {
  private:
-  // We wrap `AccessPathTreeDomain<Taint, TaintTreeConfiguration>`
-  // in order to properly update the collapse depth when collapsing.
+  using Map = RootPatriciaTreeAbstractPartition<TaintTree>;
 
-  using Tree = AccessPathTreeDomain<Taint, TaintTreeConfiguration>;
-
-  explicit TaintAccessPathTree(Tree tree) : tree_(std::move(tree)) {}
+  explicit TaintAccessPathTree(Map map) : map_(std::move(map)) {}
 
  public:
   /* Create the bottom (empty) taint access path tree. */
-  TaintAccessPathTree() : tree_(Tree::bottom()) {}
+  TaintAccessPathTree() : map_(Map::bottom()) {}
 
   explicit TaintAccessPathTree(
-      std::initializer_list<std::pair<AccessPath, Taint>> edges)
-      : tree_(std::move(edges)) {}
+      std::initializer_list<std::pair<AccessPath, Taint>> edges);
 
   INCLUDE_DEFAULT_COPY_CONSTRUCTORS_AND_ASSIGNMENTS(TaintAccessPathTree)
 
-  INCLUDE_ABSTRACT_DOMAIN_METHODS(TaintAccessPathTree, Tree, tree_)
+  INCLUDE_ABSTRACT_DOMAIN_METHODS(TaintAccessPathTree, Map, map_)
 
   TaintTree read(Root root) const;
 
   template <typename Propagate> // Taint(Taint, Path::Element)
   TaintTree read(const AccessPath& access_path, Propagate&& propagate) const {
-    return TaintTree(
-        tree_.read(access_path, std::forward<Propagate>(propagate)));
+    map_.get(access_path.root())
+        .read(access_path.path(), std::forward<Propagate>(propagate));
   }
 
+  /* Return the subtree at the given access path. */
   TaintTree read(const AccessPath& access_path) const;
 
+  /**
+   * Return the subtree at the given path.
+   *
+   * Elements are NOT propagated down to children.
+   */
   TaintTree raw_read(const AccessPath& access_path) const;
 
   /* Write taint at the given access path. */
@@ -62,19 +64,53 @@ class TaintAccessPathTree final
    */
   template <typename Visitor> // void(const AccessPath&, const Taint&)
   void visit(Visitor&& visitor) const {
-    return tree_.visit(std::forward<Visitor>(visitor));
+    static_assert(
+        std::is_same_v<
+            decltype(visitor(
+                std::declval<const AccessPath>(), std::declval<const Taint>())),
+            void>);
+    mt_assert(!is_top());
+
+    for (const auto& [root, taint_tree] : map_) {
+      auto access_path = AccessPath(root);
+      visit_internal(
+          access_path, taint_tree.tree_, std::forward<Visitor>(visitor));
+    }
   }
 
+ private:
+  template <typename Visitor> // void(const AccessPath&, const TaintTree&)
+  static void visit_internal(
+      AccessPath& access_path,
+      const TaintTree::Tree& tree,
+      Visitor&& visitor) {
+    if (!tree.root().is_bottom()) {
+      visitor(access_path, tree.root());
+    }
+
+    for (const auto& [path_element, subtree] : tree.successors()) {
+      access_path.append(path_element);
+      visit_internal(access_path, subtree, visitor);
+      access_path.pop_back();
+    }
+  }
+
+ public:
   /* Return the list of pairs (access path, taint) in the tree. */
   std::vector<std::pair<AccessPath, const Taint&>> elements() const;
 
   /* Return the list of pairs (root, taint tree) in the tree. */
-  std::vector<std::pair<Root, TaintTree>> roots() const;
+  std::vector<std::pair<Root, const TaintTree&>> roots() const;
 
   /* Apply the given function on all taint. */
   template <typename Function> // Taint(Taint)
   void transform(Function&& f) {
-    tree_.transform(std::forward<Function>(f));
+    static_assert(std::is_same_v<decltype(f(std::declval<Taint&&>())), Taint>);
+
+    map_.transform([f = std::forward<Function>(f)](TaintTree tree) {
+      tree.transform(f);
+      return tree;
+    });
   }
 
   /* Collapse children that have more than `max_leaves` leaves. */
@@ -93,12 +129,21 @@ class TaintAccessPathTree final
           is_valid,
       const std::function<Accumulator(const Root&)>& initial_accumulator,
       const FeatureMayAlwaysSet& broadening_features) {
-    tree_.collapse_invalid_paths(
-        is_valid, initial_accumulator, [&broadening_features](Taint taint) {
-          taint.add_locally_inferred_features(broadening_features);
-          taint.update_maximum_collapse_depth(CollapseDepth::zero());
-          return taint;
-        });
+    Map new_map;
+    for (const auto& [root, taint_tree] : map_) {
+      auto copy = taint_tree.tree_;
+      copy.collapse_invalid_paths(
+          is_valid,
+          /* accumulator */ initial_accumulator(root),
+          /* transform_on_collapse */ [&broadening_features](Taint taint) {
+            taint.add_locally_inferred_features(broadening_features);
+            taint.update_maximum_collapse_depth(CollapseDepth::zero());
+            return taint;
+          });
+      new_map.set(root, TaintTree(std::move(copy)));
+    }
+
+    map_ = new_map;
   }
 
   /**
@@ -118,22 +163,30 @@ class TaintAccessPathTree final
   void shape_with(
       MakeMold&& make_mold, // Taint(Taint)
       const FeatureMayAlwaysSet& broadening_features) {
-    tree_.shape_with(
-        std::forward<MakeMold>(make_mold), [&broadening_features](Taint taint) {
-          taint.add_locally_inferred_features(broadening_features);
-          taint.update_maximum_collapse_depth(CollapseDepth::zero());
-          return taint;
-        });
+    static_assert(
+        std::is_same_v<decltype(make_mold(std::declval<Taint&&>())), Taint>);
+
+    map_.transform([make_mold = std::forward<MakeMold>(make_mold),
+                    &broadening_features](const TaintTree& taint_tree) {
+      auto mold = taint_tree.tree_;
+      mold.transform(make_mold);
+
+      auto copy = taint_tree.tree_;
+      copy.shape_with(mold, [&broadening_features](Taint taint) {
+        taint.add_locally_inferred_features(broadening_features);
+        taint.update_maximum_collapse_depth(CollapseDepth::zero());
+        return taint;
+      });
+      return TaintTree(std::move(copy));
+    });
   }
 
   friend std::ostream& operator<<(
       std::ostream& out,
-      const TaintAccessPathTree& tree) {
-    return out << "TaintAccessPathTree" << tree.tree_;
-  }
+      const TaintAccessPathTree& tree);
 
  private:
-  Tree tree_;
+  Map map_;
 };
 
 } // namespace marianatrench
