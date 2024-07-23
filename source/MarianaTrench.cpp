e8184ef617568c4a83791ce3f1a920154f6ad43e
Zeyad Tarek <zeyad.ta01@gmail.com>
2024-07-23 15:59:12 +0300

    Remove the intermediate variable

--- source/MarianaTrench.cpp
+++ source/MarianaTrench.cpp
@@ -424,8 +424,8 @@ std::vector<std::string> filter_existing_jars(
 void MarianaTrench::run(const program_options::variables_map& variables) {
   Context context;
   std::string json_file_path = variables["config"].as<std::string>();
-  std::unique_ptr<Options> options_from_file = Options::options_from_json_file(json_file_path);
-  context.options = std::move(options_from_file);
+
+  context.options = Options::options_from_json_file(json_file_path);
   const auto& options = *context.options;
 
   if (auto heuristics_path = options.heuristics_path()) {
