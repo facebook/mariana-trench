f7513c89b60bfc96e455fb804c1a0f5a8c3edcfa
Anwesh Tuladhar <atuladhar@meta.com>
2024-09-12 19:06:59 -0700

    Add mock implementations for kotlin Intrinsics for integration tests
    Summary:
    As titled, this diff adds mock implementations for kotlin's `Intrinsics;.check*` apis similar to what we do for android libraries. This is useful to keep the integration tests consistent with analysis of kotlin code of apks. We can add other required internal apis as necessary.
    
    See file: `Intrinsics.java` rest are generated changes for methods defined in that file.
    
    Reviewed By: arthaud
    
    Differential Revision: D62538669
    
    fbshipit-source-id: 3d0e90fa99723f6c375abb5293f7be609e79f7e4

---
+++ source/tests/integration/end-to-end/library_classes/Intrinsics.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package kotlin.jvm.internal;
+
+public class Intrinsics {
+  private Intrinsics() {}
+
+  public static void checkNotNull(Object object) {
+    if (object == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkNotNull(Object object, String message) {
+    if (object == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkExpressionValueIsNotNull(Object value, String expression) {
+    if (value == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkNotNullExpressionValue(Object value, String expression) {
+    if (value == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkReturnedValueIsNotNull(
+      Object value, String className, String methodName) {
+    if (value == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkReturnedValueIsNotNull(Object value, String message) {
+    if (value == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkFieldIsNotNull(Object value, String className, String fieldName) {
+    if (value == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkFieldIsNotNull(Object value, String message) {
+    if (value == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkParameterIsNotNull(Object value, String paramName) {
+    if (value == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkNotNullParameter(Object value, String paramName) {
+    if (value == null) {
+      throw new RuntimeException();
+    }
+  }
+
+  public static void checkHasClass(String internalName) {
+    throw new RuntimeException();
+  }
+
+  public static void checkHasClass(String internalName, String requiredVersion) {
+    throw new RuntimeException();
+  }
+}
