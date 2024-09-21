19695f83847abfaa65714ed401e6dc8772ea0ecc
Anwesh Tuladhar <atuladhar@meta.com>
2024-09-20 17:13:15 -0700

    Add start and end to positions::to_string
    Summary: As titled.
    
    Reviewed By: arthaud
    
    Differential Revision: D63036894
    
    fbshipit-source-id: d9f9534a6093fbdd7a5998aefb45df3f1bca278f

--- source/Position.cpp
+++ source/Position.cpp
@@ -45,8 +45,15 @@ std::string Position::to_string() const {
     out.append(fmt::format("path=`{}`,", show(path_)));
   }
   if (line_ != k_unknown_line) {
-    out.append("line={}", line_);
+    out.append("line={},", line_);
   }
+  if (start_ != k_unknown_start) {
+    out.append("start={},", start_);
+  }
+  if (end_ != k_unknown_end) {
+    out.append("end={}", end_);
+  }
+
   out.append(")");
 
   return out;
