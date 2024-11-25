6df2c341071dca10371a2a3f06415495f9b7927b
Anwesh Tuladhar <atuladhar@meta.com>
2024-11-25 13:45:40 -0800

    Add PointsToTree
    Summary:
    This diff adds an `PointsToTree` abstract domain which wraps a  `AbstractTreeDomain<PointsToSet>`. The points-to-tree has different semantics compared to the taint tree as elements are not propagated down to the children.
    
    - So for reads, we only provide: `raw_read()` and `raw_read_max_path()` apis.
    - The `write()`s are the same.
    
    Reviewed By: arthaud
    
    Differential Revision: D64933072
    
    fbshipit-source-id: 60b2a19c66747cc185b043ab85f29058300621e8

---
+++ source/PointsToTree.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <mariana-trench/PointsToTree.h>
+
+namespace marianatrench {
+
+PointsToTree PointsToTree::raw_read(const Path& path) const {
+  return PointsToTree{tree_.raw_read(path)};
+}
+
+std::pair<Path, PointsToTree> PointsToTree::raw_read_max_path(
+    const Path& path) const {
+  auto [remaining_path, tree] = tree_.raw_read_max_path(path);
+  return {remaining_path, PointsToTree(std::move(tree))};
+}
+
+void PointsToTree::write(
+    const Path& path,
+    const PointsToSet& points_to_set,
+    UpdateKind kind) {
+  tree_.write(path, points_to_set, kind);
+}
+
+void PointsToTree::write(const Path& path, PointsToTree tree, UpdateKind kind) {
+  tree_.write(path, std::move(tree.tree_), kind);
+}
+
+std::ostream& operator<<(std::ostream& out, const PointsToTree& tree) {
+  return out << "PointsToTree(tree=" << tree.tree_ << ")";
+}
+
+} // namespace marianatrench
---
+++ source/PointsToTree.h
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <sparta/PatriciaTreeMapAbstractPartition.h>
+
+#include <mariana-trench/AbstractTreeDomain.h>
+#include <mariana-trench/IncludeMacros.h>
+#include <mariana-trench/MemoryLocation.h>
+#include <mariana-trench/MemoryLocationEnvironment.h>
+#include <mariana-trench/PointsToSet.h>
+
+namespace marianatrench {
+
+struct PointsToTreeConfiguration {
+  static std::size_t max_tree_height_after_widening() {
+    // Using the Heuristics::propagation_output_path_tree_widening_height, i.e.
+    // the maximum height of the output path tree of propagations after
+    // widening as this will be the maximum level of aliasing we will be able to
+    // track through propagations.
+    return Heuristics::singleton()
+        .propagation_output_path_tree_widening_height();
+  }
+
+  static PointsToSet transform_on_widening_collapse(
+      PointsToSet /* points_to_set */) {
+    mt_unreachable(); // Not implemented.
+  }
+
+  static PointsToSet transform_on_sink(PointsToSet /* points_to_set */) {
+    // PointsToTree does not propagate the points-to set down to children.
+    return PointsToSet::bottom();
+  }
+
+  static PointsToSet transform_on_hoist(PointsToSet /* points_to_set */) {
+    mt_unreachable(); // Not implemented.
+  }
+};
+
+class PointsToTree final : public sparta::AbstractDomain<PointsToTree> {
+ private:
+  using Tree = AbstractTreeDomain<PointsToSet, PointsToTreeConfiguration>;
+
+ public:
+  /* Create the bottom (empty) points-to tree. */
+  PointsToTree() : tree_(Tree::bottom()) {}
+
+ private:
+  explicit PointsToTree(Tree tree) : tree_(std::move(tree)) {}
+
+ public:
+  explicit PointsToTree(PointsToSet points_to_set)
+      : tree_(std::move(points_to_set)) {}
+
+  INCLUDE_ABSTRACT_DOMAIN_METHODS(PointsToTree, Tree, tree_)
+
+ public:
+  const PointsToSet& root() const {
+    return tree_.root();
+  }
+
+  const Tree::Map& successors() const {
+    return tree_.successors();
+  }
+
+  /**
+   * Return the points-to set at the given path wrapped as a PointsToTree.
+   *
+   * Points-to set are _not_ propagated down to children.
+   */
+  PointsToTree raw_read(const Path& path) const;
+
+  std::pair<Path, PointsToTree> raw_read_max_path(const Path& path) const;
+
+  /* Write the given memory_location as a points-to set at the given path. */
+  void write(
+      const Path& path,
+      const MemoryLocation* memory_location,
+      UpdateKind kind);
+
+  /* Write the given points-to set at the given path. */
+  void
+  write(const Path& path, const PointsToSet& points_to_set, UpdateKind kind);
+
+  /* Write the given points-to tree at the given path. */
+  void write(const Path& path, PointsToTree tree, UpdateKind kind);
+
+  /**
+   * Iterate on all non-empty points-to set in the tree.
+   *
+   * When visiting the tree, points-to set do not include their ancestors.
+   */
+  template <typename Visitor> // void(const Path&, const PointsToSet&)
+  void visit(Visitor&& visitor) const {
+    return tree_.visit(std::forward<Visitor>(visitor));
+  }
+
+  /* Apply the given function on all PointsToSet. */
+  template <typename Function> // PointsToSet(PointsToSet)
+  void transform(Function&& f) {
+    tree_.transform(std::forward<Function>(f));
+  }
+
+  friend std::ostream& operator<<(std::ostream& out, const PointsToTree& tree);
+
+ private:
+  Tree tree_;
+};
+
+} // namespace marianatrench
