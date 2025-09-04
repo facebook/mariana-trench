3158eb2f226ea7633e00ee55b63caa2179506cba
Anwesh Tuladhar <atuladhar@meta.com>
2025-09-04 11:42:08 -0700

    Add partial kinds sanitizers test
    Summary: We allow specifying partial kinds in sanitizer dsl but do not correctly sanitize them during analysis. This diff adds a test to demonstrate this problem.
    
    Reviewed By: jeremy-neale
    
    Differential Revision: D81634976
    
    fbshipit-source-id: 8708ddba74bb1bb3ec867e9b4d0e64190a9683fd

---
+++ source/tests/integration/end-to-end/code/sanitizers/PartialSinkSanitizers.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package com.facebook.marianatrench.integrationtests;
+
+public class PartialSinkSanitizers {
+  public static Object sourceA() {
+    return new Object();
+  }
+
+  public static Object sourceB() {
+    return new Object();
+  }
+
+  public static void multiSinks(Object a, Object b) {}
+
+  public void triggeredPartialSinkForSourceA(Object object) {
+    // Expect triggered PartialSink for partial label "arg0" to be
+    // inferred for triggeredPartialSinkForSourceA()
+    PartialSinkSanitizers.multiSinks(object, PartialSinkSanitizers.sourceB());
+  }
+
+  public void triggeredPartialSinkForSourceB(Object object) {
+    // Expect triggered PartialSink for partial label "arg1" to be
+    // inferred for triggeredPartialSinkForSourceB()
+    PartialSinkSanitizers.multiSinks(PartialSinkSanitizers.sourceA(), object);
+  }
+
+  public void sanitizedPartialSinkForSourceA(Object object) {
+    // Expect no triggered PartialSink to be inferred for sanitizedPartialSinkForSourceA().
+    PartialSinkSanitizers.multiSinks(object, PartialSinkSanitizers.sourceB());
+  }
+
+  public void sanitizedPartialSinkForSourceB(Object object) {
+    // Expect no triggered PartialSink to be inferred for sanitizedPartialSinkForSourceB().
+    PartialSinkSanitizers.multiSinks(PartialSinkSanitizers.sourceA(), object);
+  }
+
+  public void testMultiSourceIssue1() {
+    triggeredPartialSinkForSourceA(PartialSinkSanitizers.sourceA());
+  }
+
+  public void testMultiSourceIssue2() {
+    triggeredPartialSinkForSourceB(PartialSinkSanitizers.sourceB());
+  }
+
+  public void testMultiSourceNoIssue1() {
+    // Currently a False Positive
+    sanitizedPartialSinkForSourceA(PartialSinkSanitizers.sourceA());
+  }
+
+  public void testMultiSourceNoIssue2() {
+    // Currently a False Positive
+    sanitizedPartialSinkForSourceB(PartialSinkSanitizers.sourceB());
+  }
+}
